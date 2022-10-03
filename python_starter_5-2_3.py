5f1f207d4657ba9ba79ae5c8b3c53debc354185d
Ihor Abakumov <ihor.abakumov@gmail.com>
2022-10-03 19:52:23 +0300

    Home Work - 5 (2, 3)

---
+++ python_starter_5-2_3.py
@@ -0,0 +1,14 @@
+# HW - 1
+a = [1, 9, 10, 12, 2, 1, 4 ,3]
+b = []
+for i in a:
+    if i < 6:
+        b.append(i)
+print (b)
+print("-------------------------")
+
+# HW - 2
+a = ['red', 'yellow', 'blue', 'bread']
+a = a.pop(3)
+print (a)
+
